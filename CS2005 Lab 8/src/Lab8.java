f64323da5f8a8f8f3bfbc2cdef060a18fa8fd9fb
cs17kkd <cs17kkd@6XD0NK2.ikb.brunel.ac.uk>
2018-12-03 16:00:31 +0000

    Committed

---
+++ CS2005 Lab 8/src/Lab8.java
@@ -0,0 +1,17 @@
+
+public class Lab8 
+{
+	
+	public static void main(String args[])
+	{
+//		for(int i=0;i<10;++i)
+//		{
+//			int x = CS2004.UI(-1, 11);
+//			System.out.println(x);
+//		}
+		
+		System.out.println(ScalesSolution.RandomBinaryString(20));
+		
+	}
+	
+}
