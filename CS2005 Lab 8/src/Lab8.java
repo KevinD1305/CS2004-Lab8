8a817a67a1268c7b86155b6de03c23d7fbd40107
CS17KKD <CS17KKD@6WMXMK2.ikb.brunel.ac.uk>
2018-12-04 18:28:32 +0000

    Lab8

--- CS2005 Lab 8/src/Lab8.java
+++ CS2005 Lab 8/src/Lab8.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Lab8 
 {
@@ -9,9 +10,16 @@ public class Lab8
 //			int x = CS2004.UI(-1, 11);
 //			System.out.println(x);
 //		}
+		ArrayList<Double> array = new ArrayList<Double>();	
+		array.add(1.0);
+		array.add(2.0);
+		array.add(3.0);
+		array.add(4.0);
+		array.add(10.0);
 		
-		System.out.println(ScalesSolution.RandomBinaryString(20));
-		
+		ScalesSolution INSTANCE = new ScalesSolution(5);
+		//Generates a solution of n.
+		System.out.println(INSTANCE.ScalesFitness(array));
 	}
 	
 }
